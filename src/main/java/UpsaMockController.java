95570c2ef45ae44c6065e1af7e649175512d222a
Csaba Valyi <csaba_valyi@epam.com>
2014-04-16 10:37:29 +0200

    escaped json object with double quote mark

--- src/main/java/UpsaMockController.java
+++ src/main/java/UpsaMockController.java
@@ -23,100 +23,100 @@ public class UpsaMockController {
         if("John_Doe@epam.com".equals(email)){
             employee = "["+
              "{"+
-               "'uid': '620000',"+
-               "'employeeId': '32563187465294591',"+
-               "'primarySkill': 'JAVA',"+
-               "'jobFunctionId': '1',"+
-               "'fired': '',"+
-               "'phone': '44444',"+
-               "'fullParticipationList': ["+
+               "\"uid\": \"620000\","+
+               "\"employeeId\": \"32563187465294591\","+
+               "\"primarySkill\": \"JAVA\","+
+               "\"jobFunctionId\": \"1\","+
+               "\"fired\": \"\","+
+               "\"phone\": \"44444\","+
+               "\"fullParticipationList\": ["+
                  "{"+
-                   "'participationRoleName': 'Dummy',"+
-                   "'participationRoleId': '1',"+
-                   "'projectStateId': '2',"+
-                   "'projectId': '7',"+
-                   "'projectStateName': 'Production',"+
-                   "'projectName': 'TJHooker'"+
+                   "\"participationRoleName\": \"Dummy\","+
+                   "\"participationRoleId\": \"1\","+
+                   "\"projectStateId\": \"2\","+
+                   "\"projectId\": \"7\","+
+                   "\"projectStateName\": \"Production\","+
+                   "\"projectName\": \"TJHooker\""+
                  "}"+
                "],"+
-               "'locationId': '2',"+
-               "'dismissal': {"+
-                 "'activeDismissalDate': '',"+
-                 "'isPendingDismissal': 'false'"+
+               "\"locationId\": \"2\","+
+               "\"dismissal\": {"+
+                 "\"activeDismissalDate\": \"\","+
+                 "\"isPendingDismissal\": \"false\""+
                "},"+
-               "'proposalList': ["+
+               "\"proposalList\": ["+
                  "{"+
-                   "'positionBillingName': 'DEV1',"+
-                   "'created': '2013-02-05',"+
-                   "'positionId': '2',"+
-                   "'projectId': '7',"+
-                   "'isBooked': 'true',"+
-                   "'positionBillingId': '2',"+
-                   "'projectName': 'TJHooker',"+
-                   "'comments': ["+
+                   "\"positionBillingName\": \"DEV1\","+
+                   "\"created\": \"2013-02-05\","+
+                   "\"positionId\": \"2\","+
+                   "\"projectId\": \"7\","+
+                   "\"isBooked\": \"true\","+
+                   "\"positionBillingId\": \"2\","+
+                   "\"projectName\": \"TJHooker\","+
+                   "\"comments\": ["+
                      "{"+
-                       "'created': '2014-01-28',"+
-                       "'userId': '77777',"+
-                       "'action': {"+
-                         "'id': '1',"+
-                         "'authority': 'none',"+
-                         "'description': '',"+
-                         "'name': 'Randy Manager'"+
+                       "\"created\": \"2014-01-28\","+
+                       "\"userId\": \"77777\","+
+                       "\"action\": {"+
+                         "\"id\": \"1\","+
+                         "\"authority\": \"none\","+
+                         "\"description\": \"\","+
+                         "\"name\": \"Randy Manager\""+
                        "},"+
-                       "'userName': 'RandyM',"+
-                       "'notes': 'Awesome workforce',"+
-                       "'itemId': '5',"+
-                       "'commentId': '5',"+
-                       "'itemTypeId': '1'"+
+                       "\"userName\": \"RandyM\","+
+                       "\"notes\": \"Awesome workforce\","+
+                       "\"itemId\": \"5\","+
+                       "\"commentId\": \"5\","+
+                       "\"itemTypeId\": \"1\""+
                      "}"+
                    "]"+
                  "}"+
                "],"+
-               "'probation': {"+
-                 "'actualEndDate': '2013-06-05',"+
-                 "'probationStatus': {"+
-                   "'id': '1',"+
-                   "'name': 'string'"+
+               "\"probation\": {"+
+                 "\"actualEndDate\": \"2013-06-05\","+
+                 "\"probationStatus\": {"+
+                   "\"id\": \"1\","+
+                   "\"name\": \"string\""+
                  "},"+
-                 "'originalEndDate': '2013-06-05',"+
-                 "'endDate': '2013-06-05',"+
-                 "'prolongationDate': ''"+
+                 "\"originalEndDate\": \"2013-06-05\","+
+                 "\"endDate\": \"2013-06-05\","+
+                 "\"prolongationDate\": \"\""+
                "},"+
-               "'statusId': '1',"+
-               "'title': 'Software Engineer',"+
-               "'bookedCount': {},"+
-               "'hired': '2013-03-05',"+
-               "'primarySkillId': 'JAVA',"+
-               "'gender': 'male',"+
-               "'reporterId': '2',"+
-               "'proposalsCount': '1',"+
-               "'jobFunction': 'Development Level 2',"+
-               "'participationList': ["+
+               "\"statusId\": \"1\","+
+               "\"title\": \"Software Engineer\","+
+               "\"bookedCount\": {},"+
+               "\"hired\": \"2013-03-05\","+
+               "\"primarySkillId\": \"JAVA\","+
+               "\"gender\": \"male\","+
+               "\"reporterId\": \"2\","+
+               "\"proposalsCount\": \"1\","+
+               "\"jobFunction\": \"Development Level 2\","+
+               "\"participationList\": ["+
                  "{"+
-                   "'participationRole': 'Developer',"+
-                   "'projectId': '3',"+
-                   "'lastMonth': 'true',"+
-                   "'projectName': 'AnotherAwesomeProject',"+
-                   "'lastWorkload': '1.0'"+
+                   "\"participationRole\": \"Developer\","+
+                   "\"projectId\": \"3\","+
+                   "\"lastMonth\": \"true\","+
+                   "\"projectName\": \"AnotherAwesomeProject\","+
+                   "\"lastWorkload\": \"1.0\""+
                  "}"+
                "],"+
-               "'status': 'hired',"+
-               "'managerName': 'Randy Manager',"+
-               "'categoryId': '2',"+
-               "'fullLocation': 'Budapest',"+
-               "'reporterName': 'Ann Recruiter',"+
-               "'workstation': 'EPHUBUDW0007',"+
-               "'isActive': 'true',"+
-               "'category': 'dunno',"+
-               "'email': 'John_Doe@epam.com',"+
-               "'unitName': 'string',"+
-               "'managerId': '245513',"+
-               "'fullName': 'John Doe Wasowski',"+
-               "'unitId': '777'"+
+               "\"status\": \"hired\","+
+               "\"managerName\": \"Randy Manager\","+
+               "\"categoryId\": \"2\","+
+               "\"fullLocation\": \"Budapest\","+
+               "\"reporterName\": \"Ann Recruiter\","+
+               "\"workstation\": \"EPHUBUDW0007\","+
+               "\"isActive\": \"true\","+
+               "\"category\": \"dunno\","+
+               "\"email\": \"John_Doe@epam.com\","+
+               "\"unitName\": \"string\","+
+               "\"managerId\": \"245513\","+
+               "\"fullName\": \"John Doe Wasowski\","+
+               "\"unitId\": \"777\""+
              "}"+
            "]";
         } else {
-            employee = "{'message': Object is not found, 'exception': 'com.epam.workload.common.ObjectNotFoundException'}";
+            employee = "{\"message\": Object is not found, \"exception\": \"com.epam.workload.common.ObjectNotFoundException\"}";
         }
         
         return new ResponseEntity<String>(employee, responseHeaders, HttpStatus.CREATED);
